cecd9e6e501c582f19db65fb904bd70bfa3710d4
yukichi kawada <yukichikawada@gmail.com>
2017-11-01 22:36:36 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,8 @@
 # Build your say_hello method here
 def say_hello(name=nil)
-  puts "Hello Ruby Programmer!"
+  if name == nil
+    puts "Hello Ruby Programmer!"
+  else
+    puts "Hello #{name}!"
+  end
 end
